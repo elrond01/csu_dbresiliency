61be9994c25bff47cb69f0315bab1be6079cee54
elrond01 <oe.gonzalez@gmail.com>
2022-11-06 20:53:11 -0500

    Add files via upload

---
+++ deploy.ps1
@@ -0,0 +1,55 @@
+Connect-AzAccount
+
+$adminLogin = "adminuser"
+$resourceGroupName = "rgcsuarchitecture2"
+$location = "eastus"
+
+New-AzResourceGroup -Name $resourceGroupName  -Location $location
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName  -TemplateFile ./main.bicep -administratorLogin $adminLogin
+
+$failoverGroupName = (Get-AzResource -ResourceGroupName $resourceGroupName -ResourceType Microsoft.Sql/servers).Name |  Select-Object -First 1
+$failoverGroupName = $failoverGroupName+"fog"
+
+Write-Output $failoverGroupName
+
+$subscriptionId = (Get-AzContext).Subscription.Id
+Write-Output $subscriptionId
+
+$password = Read-Host -Prompt 'Password for secondary sql server:' -AsSecureString
+$serverName = (Get-AzResource -ResourceGroupName $resourceGroupName -ResourceType Microsoft.Sql/servers).Name|  Select-Object -First 1
+$databaseName = "dbapp1"
+$drLocation = "westus"
+$drServerName = (Get-AzResource -ResourceGroupName $resourceGroupName -ResourceType Microsoft.Sql/servers).Name|  Select-Object -First 1
+$drServerName = $drServerName + "dr"
+
+# Create a secondary server in the failover region
+Write-host "Creating a secondary server in the failover region..."
+$drServer = New-AzSqlServer -ResourceGroupName $resourceGroupName `
+   -ServerName $drServerName `
+   -Location $drLocation `
+   -SqlAdministratorCredentials $(New-Object -TypeName System.Management.Automation.PSCredential `
+      -ArgumentList $adminlogin, $(ConvertTo-SecureString -String $password -AsPlainText -Force))
+$drServer
+
+# Create a failover group between the servers
+$failovergroup = Write-host "Creating a failover group between the primary and secondary server..."
+New-AzSqlDatabaseFailoverGroup `
+   -ResourceGroupName $resourceGroupName `
+   -ServerName $serverName `
+   -PartnerServerName $drServerName  `
+   -FailoverGroupName $failoverGroupName `
+   -FailoverPolicy Automatic `
+   -GracePeriodWithDataLossHours 1
+$failovergroup
+
+# Add the database to the failover group
+Write-host "Adding the database to the failover group..."
+Get-AzSqlDatabase `
+   -ResourceGroupName $resourceGroupName `
+   -ServerName $serverName `
+   -DatabaseName $databaseName | `
+Add-AzSqlDatabaseToFailoverGroup `
+   -ResourceGroupName $resourceGroupName `
+   -ServerName $serverName `
+   -FailoverGroupName $failoverGroupName
+Write-host "Successfully added the database to the failover group..."
