97ebe8f2f6990f556d3131cc6d8d23d704fdde54
elrond01 <oe.gonzalez@gmail.com>
2022-11-08 19:10:40 -0500

    Adicion FAILOVER.Ps1

---
+++ failover.ps1
@@ -0,0 +1,28 @@
+# Set variables
+$resourceGroupName = "rgdbresiliency4"
+$serverName = "dpozw536ljp6s"
+$failoverGroupName = $serverName+"fog"
+$drServerName = $serverName + "dr"
+
+# Check role of secondary replica
+Write-host "Confirming the secondary replica is secondary...."
+(Get-AzSqlDatabaseFailoverGroup `
+   -FailoverGroupName $failoverGroupName `
+   -ResourceGroupName $resourceGroupName `
+   -ServerName $drServerName).ReplicationRole
+
+# Failover to secondary server
+Write-host "Failing over failover group to the secondary..."
+Switch-AzSqlDatabaseFailoverGroup `
+   -ResourceGroupName $resourceGroupName `
+   -ServerName $drServerName `
+   -FailoverGroupName $failoverGroupName
+Write-host "Failed failover group successfully to" $drServerName
+
+# Revert failover to primary server
+Write-host "Failing over failover group to the primary...."
+Switch-AzSqlDatabaseFailoverGroup `
+   -ResourceGroupName $resourceGroupName `
+   -ServerName $serverName `
+   -FailoverGroupName $failoverGroupName
+Write-host "Failed failover group successfully back to" $serverName
