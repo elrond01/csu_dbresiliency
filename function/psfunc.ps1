26d9b41732eeefcfc450ee375d66a4529fdbe427
aperezv01 <83298224+aperezv01@users.noreply.github.com>
2022-11-11 15:11:08 -0500

    Update psfunc.ps1

--- function/psfunc.ps1
+++ function/psfunc.ps1
@@ -1,3 +1,2 @@
 Register-AzResourceProvider -ProviderNamespace Microsoft.Cdn
-New-AzResourceGroup -Name exampleRG -Location eastus
-New-AzResourceGroupDeployment -ResourceGroupName exampleRG -TemplateFile .\functions_fd.bicep -appInsightsLocation "east us"
+New-AzResourceGroupDeployment -ResourceGroupName rgdbresiliency -TemplateFile .\functions_fd.bicep -appInsightsLocation "east us"
