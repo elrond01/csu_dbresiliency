70d587290b752b9206892f98048d565f113c626c
aperezv01 <83298224+aperezv01@users.noreply.github.com>
2022-11-11 12:05:35 -0500

    Delete psfunc.ps1

--- psfunc.ps1
+++
@@ -1,3 +0,0 @@
-Register-AzResourceProvider -ProviderNamespace Microsoft.Cdn
-New-AzResourceGroup -Name exampleRG -Location eastus
-New-AzResourceGroupDeployment -ResourceGroupName exampleRG -TemplateFile .\functions_fd.bicep -appInsightsLocation "east us"
