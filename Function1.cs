aaa8186988c9a69f030dc1241863eafefb98d727
Andres Perez Velez <aperezvelez@microsoft.com>
2022-11-15 15:59:37 -0500

    commit

--- Function1.cs
+++ Function1.cs
@@ -1,35 +1,45 @@
-using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-
-namespace SQLFuncApp
+using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+namespace AzureFunction
 {
-    public static class Function1
+    public static class GetCustomerInfoFunction
     {
-        [FunctionName("Function1")]
+        [FunctionName("GetCustomerInfo")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            string id = req.Query["id"];
+            var results = new List<string>();
+            var str = Environment.GetEnvironmentVariable("SqlConnection");
+            var reg = Environment.GetEnvironmentVariable("Region");
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+                conn.Open();
+                var text = "SELECT id, Name, Email " +
+                           "FROM [dbo].[Customers] " +
+                           $"Where id={id}";
+                using (SqlCommand cmd = new SqlCommand(text, conn))
+                {
+                    // Execute the command and log the # rows affected.
+                    var reader = await cmd.ExecuteReaderAsync();
+                    while (reader.Read())
+                    {
+                        results.Add(reader.GetString(1));
+                        results.Add(reader.GetString(2));
+                        results.Add(reg);
+                    }
+                }
+            }
+            return new OkObjectResult(results);
         }
     }
 }
